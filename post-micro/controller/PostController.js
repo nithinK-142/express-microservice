f710b8f88f78f15528060f818fbfec4231549629
nithinK-142 <nithin.sagar.359@gmail.com>
2024-06-04 17:58:13 +0530

    add: posts with users info.

--- post-micro/controller/PostController.js
+++ post-micro/controller/PostController.js
@@ -1,10 +1,43 @@
 import prisma from "../config/db.config.js";
+import axios from "axios";
 
 class PostController {
   static async index(req, res) {
     try {
       const posts = await prisma.post.findMany({});
-      return res.status(200).json({ posts });
+
+      let userIds = [];
+      posts.forEach((post) => userIds.push(post.userId));
+
+      const response = await axios.post(
+        `${process.env.AUTH_MICRO_URL}/api/getUsers`,
+        userIds
+      );
+
+      // let postWithUsers = await Promise.all(
+      //   posts.map(async (post) => {
+      //     const user = await response.data.users.find((item) => item.id === post.userId);
+      //     return {
+      //       ...post,
+      //       user,
+      //     };
+      //   })
+      // );
+
+      const users = {};
+      response.data.users.forEach((user) => (users[user.id] = user));
+
+      let postWithUsers = await Promise.all(
+        posts.map((post) => {
+          const user = users[post.userId];
+          return {
+            ...post,
+            user,
+          };
+        })
+      );
+
+      return res.status(200).json({ postWithUsers });
     } catch (error) {
       return res.status(500).json({ error: "Post not found" });
     }
