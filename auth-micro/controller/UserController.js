00e1a47f1002a63c7ec1531642100a4aa3281b78
nithinK-142 <nithin.sagar.359@gmail.com>
2024-06-04 16:40:50 +0530

    add: getUsers route.

--- auth-micro/controller/UserController.js
+++ auth-micro/controller/UserController.js
@@ -25,6 +25,28 @@ class UserController {
       return res.status(404).json({ error: "User not found!!" });
     }
   }
+
+  static async getUsers(req, res) {
+    try {
+      const { userIds } = req.body;
+
+      const users = await prisma.user.findMany({
+        where: {
+          id: {
+            in: userIds,
+          },
+        },
+        select: {
+          id: true,
+          name: true,
+          email: true,
+        },
+      });
+      return res.status(200).json({ users });
+    } catch (error) {
+      return res.status(404).json({ error: "User not found!!" });
+    }
+  }
 }
 
 export default UserController;
--- auth-micro/routes/userRoutes.js
+++ auth-micro/routes/userRoutes.js
@@ -4,5 +4,6 @@ import UserController from "../controller/UserController.js";
 const router = Router();
 
 router.get("/getUser/:id", UserController.getUser);
+router.post("/getUsers", UserController.getUsers);
 
 export default router;
